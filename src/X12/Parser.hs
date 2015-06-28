778c9257d13d6317fd6c3386a355f4a2b5bfde3a
Alex Kyllo <alex.kyllo@gmail.com>
2015-06-27 18:43:50 -0700

    break Value type and parsers into separate file/module

--- src/X12/Parser.hs
+++ src/X12/Parser.hs
@@ -3,79 +3,14 @@
 
 module X12.Parser where
 import Prelude hiding (concat, takeWhile, take)
-import Data.Text (Text(..), pack)
-import Data.Time.Calendar (Day(..), fromGregorian)
-import Data.Time.LocalTime (TimeOfDay(..))
-import Data.Time.Format
+import X12.Parser.Value
+import Data.Text (Text)
 import Data.Attoparsec.Text
-import Data.Scientific (Scientific)
 import Control.Applicative (pure, many, (<*),(*>),(<*>),(<|>),(<$>))
 
 type Element = Text
 type Identifier = Text
 type FunctionalGroup = [TransactionSet]
-
-data Value = AN Text
-             | ID Text
-             | DT Day
-             | TM TimeOfDay
-             | R  Scientific
-             | N  Integer
-             deriving (Eq, Show)
-
-value :: Text -> Parser Value
-value valtype = case valtype of
-  "DT" -> DT <$> (dayParser8 <|> dayParser6)
-  "TM" -> TM <$> (timeParser6 <|> timeParser4)
-  "R"  -> R  <$> scientific
-  "N"  -> N  <$> (signed decimal)
-  "ID" -> ID <$> takeText
-  otherwise -> AN <$> takeText
-
-parseDT :: Parser Value
-parseDT = DT <$> (dayParser8 <|> dayParser6)
-
-parseTM :: Parser Value
-parseTM = TM <$> (timeParser6 <|> timeParser4)
-
-parseR :: Parser Value
-parseR = R <$> scientific
-
-parseN :: Parser Value
-parseN = N <$> (signed decimal)
-
-parseID :: Char -> Char -> Parser Value
-parseID sep term = ID <$> takeWhile (`notElem` [sep, term])
-
-parseAN :: Char -> Char -> Parser Value
-parseAN sep term = AN <$> takeWhile (`notElem` [sep, term])
-
-dayParser8 :: Parser Day
-dayParser8 = do
-  yyyy <- count 4 digit
-  mm <- count 2 digit
-  dd <- count 2 digit
-  return $ fromGregorian (read yyyy) (read mm)  (read dd)
-
-dayParser6 :: Parser Day
-dayParser6 = do
-  yy <- count 2 digit
-  mm <- count 2 digit
-  dd <- count 2 digit
-  return $ fromGregorian (read ("20" ++ yy)) (read mm) (read dd)
-
-timeParser6 :: Parser TimeOfDay
-timeParser6 = do
-  hh <- count 2 digit
-  mm <- count 2 digit
-  ss <- count 2 digit
-  return $ TimeOfDay (read hh) (read mm) (read ss)
-
-timeParser4 :: Parser TimeOfDay
-timeParser4 = do
-  hh <- count 2 digit
-  mm <- count 2 digit
-  return $ TimeOfDay (read hh) (read mm) 00
 
 parseISA :: Parser [Value]
 parseISA = do
