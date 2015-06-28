44ae9b1516a0639cf6c0788a8d40cb802759dc74
Alex Kyllo <alex.kyllo@gmail.com>
2015-06-27 20:01:29 -0700

    add test for parseDT on 4 digit date

--- test/X12/Parser/ValueSpec.hs
+++ test/X12/Parser/ValueSpec.hs
@@ -5,7 +5,7 @@ module X12.Parser.ValueSpec where
 
 import SpecHelper
 import Data.Text (Text)
-import Data.Time.Calendar (Day(..), fromGregorian)
+import Data.Time.Calendar (fromGregorian)
 
 main :: IO ()
 main = hspec spec
@@ -16,4 +16,8 @@ spec = do
     context "Parse a 6-digit date" $ do
       it "parses 20150627 into Day 2015-06-27" $ do
         ("20150627" :: Text) ~> parseDT
+        `shouldParse` (DT (fromGregorian 2015 6 27))
+    context "Parse a 4-digit date" $ do
+      it "parses 150627 into Day 2015-06-27" $ do
+        ("150627" :: Text) ~> parseDT
         `shouldParse` (DT (fromGregorian 2015 6 27))
