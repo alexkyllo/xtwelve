e8de3739de990679bc4275327283051bef450524
Alex Kyllo <alex.kyllo@gmail.com>
2015-06-28 13:44:33 -0700

    add tests for parseTM, move imports into SpecHelper

--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -1,11 +1,19 @@
 -- |
 
 module SpecHelper
-    ( module Test.Hspec
-    , module Test.Hspec.Attoparsec
-    , module X12.Parser.Value
-    ) where
+       (
+         module Test.Hspec
+       , module Test.Hspec.Attoparsec
+       , module X12.Parser.Value
+       , module Data.Text
+       , module Data.Time.Calendar
+       , module Data.Time.LocalTime
+       )
+     where
 
+import Data.Text (Text)
 import Test.Hspec
 import Test.Hspec.Attoparsec
 import X12.Parser.Value
+import Data.Time.Calendar (fromGregorian)
+import Data.Time.LocalTime (TimeOfDay(..))
--- test/X12/Parser/ValueSpec.hs
+++ test/X12/Parser/ValueSpec.hs
@@ -4,8 +4,6 @@
 module X12.Parser.ValueSpec where
 
 import SpecHelper
-import Data.Text (Text)
-import Data.Time.Calendar (fromGregorian)
 
 main :: IO ()
 main = hspec spec
@@ -13,11 +11,19 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "X12.Parser.Value" $ do
-    context "Parse a 6-digit date" $ do
+    context "Parse a 8-digit date" $ do
       it "parses 20150627 into Day 2015-06-27" $ do
         ("20150627" :: Text) ~> parseDT
         `shouldParse` (DT (fromGregorian 2015 6 27))
-    context "Parse a 4-digit date" $ do
+    context "Parse a 6-digit date" $ do
       it "parses 150627 into Day 2015-06-27" $ do
         ("150627" :: Text) ~> parseDT
         `shouldParse` (DT (fromGregorian 2015 6 27))
+    context "Parse a 6-digit time" $ do
+      it "parses 123456 into TimeOfDay 12:34:56" $ do
+        ("123456" :: Text) ~> parseTM
+        `shouldParse` (TM (TimeOfDay 12 34 56))
+    context "Parse a 4-digit time" $ do
+      it "parses 1234 into TimeOfDay 12:34:00" $ do
+        ("1234" :: Text) ~> parseTM
+        `shouldParse` (TM (TimeOfDay 12 34 0))
