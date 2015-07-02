efe38016bf8c71a46e74052eafa3761c8f4d3483
Alex Kyllo <alex.kyllo@gmail.com>
2015-07-01 22:36:10 -0700

    start adding some element type record defs for ISA segment

--- src/X12/Definitions/ElementTypes.hs
+++ src/X12/Definitions/ElementTypes.hs
@@ -1,13 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
 -- |
 
 module X12.Definitions.ElementTypes where
 import Data.Map hiding (map)
 import Data.Text
+import X12.Parser.Value
 
-data ID = ID { elementId :: Text
-             , name :: Text
-             , minLength :: Int
-             , maxLength :: Int
-             , codeList :: Map Text Text
-             , description :: Text
+data ElementVal =
+  ElementVal { elementId :: Text
+             , elementName :: Text
+             , elementValue :: Maybe Value
+             , elementMinLength :: Int
+             , elementMaxLength :: Int
+             , elementPrecision :: Maybe Int --for N and R values
+             , elementCodeList :: (Maybe (Map Text Text))
              }
+  deriving Show
+
+i01 = ElementVal { elementId = "I01"
+                 , elementName = "Authorization Information Qualifier"
+                 , elementValue = Nothing
+                 , elementMinLength = 2
+                 , elementMaxLength = 2
+                 , elementPrecision = Nothing
+                 , elementCodeList = Just $ fromList ([("00", "No Authorization Information Present (No Meaningful Information in I02)")])
+                 }
+
+i02 = ElementVal { elementId = "I02"
+                 , elementName = "Authorization Information"
+                 , elementValue = Nothing
+                 , elementMinLength = 10
+                 , elementMaxLength = 10
+                 , elementPrecision = Nothing
+                 , elementCodeList = Nothing
+                 }
--- src/X12/Parser.hs
+++ src/X12/Parser.hs
@@ -5,6 +5,7 @@ module X12.Parser where
 import Prelude hiding (concat, takeWhile, take, lookup)
 import X12.Parser.Value
 import X12.Tokenizer
+import X12.Definitions.ElementTypes
 import Data.Either
 import Data.Map hiding (map)
 import Data.Text (Text, unpack)
@@ -29,27 +30,6 @@ gsTypes = ["ID","ID","AN","AN","DT","TM","N","ID","AN"]
 
 stTypes :: [Text]
 stTypes = ["ID","ID","ID","ID","ID","AN","AN","DT","ID","ID","R","ID","ID","AN","AN","AN","ID","ID","R","DT","N","DT","N","N","DT","N","R","AN"]
-
-{-- Wishing I could do something like this:
-type ID = Text
-type AN = Text
-type DT = Day
-type TM = TimeOfDay
-type R = Scientific
-type N = Integer
-
-data SegmentT = ISA ID ID AN ID AN ID AN ID AN DT TM ID ID N ID ID AN
-              | GS ID ID AN AN DT TM N ID AN
-              | ST ID ID ID ID ID AN AN DT ID ID R ID ID AN AN AN ID ID R DT N DT N N DT N R AN
-
-getTypeParser x = case x of
-  DT _ -> DT <$> dayParser8 <|> dayParser6
-  TM _ -> TM <$> timeParser6 <|> timeParser4
-  R _  -> R <$> scientific
-  N _  -> N <$> (signed decimal)
-  ID _ -> ID <$> takeText
-  _    -> AN <$> takeText
---}
 
 data Interchange =
   Interchange { interchangeSegment :: Segment
@@ -91,19 +71,11 @@ data Loop =
        }
   deriving Show
 
-data ElementVal =
-  ElementVal { elementId :: Text
-             , elementType :: Text
-             , elementValue :: Maybe Value
-             }
-  deriving Show
-
-parseSegmentTokE :: Either String [Text] -> Either [String] [Value]
-parseSegmentTokE (Right r) = do
-  parsedElements <- fromEithers $ zipWith parseOnly (map value isaTypes) r
-  return parsedElements
-parseSegmentTokE (Left err) = error $ "A parsing error was found: " ++ err
-
+parseSegmentTokE :: Either String [Text] -> Either String [Either String Value]
+parseSegmentTokE (Right s@(x:xs)) = case getSegmentTypes x of
+  Just segTypes -> Right $ zipWith parseOnly (map value segTypes) s
+  Nothing -> Left $ "Segment definition not found: " ++ (unpack x)
+parseSegmentTokE _ = Left "Received an empty segment."
 
 parseSegmentTok :: [Text] -> Either String [Either String Value]
 parseSegmentTok s@(x:xs) = case getSegmentTypes x of
@@ -119,7 +91,9 @@ fromEithers eithers = case lefts eithers of
   [] -> Right (rights eithers)
   _ -> Left (lefts eithers)
 
-
 parseInterchangeTok :: Either String [[Text]] -> [Either String [Either String Value]]
 parseInterchangeTok (Right r) = map parseSegmentTok r
 parseInterchangeTok (Left err) = error $ "A parsing error was found: " ++ err
+
+valueToElement :: [Either String Value] -> ElementVal
+valueToElement = undefined
