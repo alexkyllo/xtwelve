51bce0dd2cdc625db39385ee3f117a0284773ea1
Alex Kyllo <alex.kyllo@gmail.com>
2015-07-16 19:09:10 -0400

    trying something with GADTs

--- src/X12/Types.hs
+++ src/X12/Types.hs
@@ -3,6 +3,7 @@
 
 module X12.Types where
 import X12.Definitions
+import X12.Values
 import Data.Time.Calendar
 import Data.Text
 
@@ -26,3 +27,7 @@ data I02 = I02 (Value AN)
 --data ISA = ISA I01 I02
 
 data IEA = IEA (Value ID)
+
+-- How to define types for DSL to generate a message?
+-- Write a function to generate an ISA SegmentVal from arguments
+-- isa seps isa01 isa02 isa03 isa04 isa05 isa06 isa07 isa08 isa09 isa10 isa11 isa12 isa13 isa14 isa15
